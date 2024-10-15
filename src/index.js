dac19d0b0a07b058dd9a617c5677f2376f6c63a0
kimthedrew <kimutai848@gmail.com>
2024-10-16 00:25:39 +0300

    movies

--- src/index.js
+++ src/index.js
@@ -25,3 +25,60 @@ function movieDetailsDisplay(movie) {
     };
 
 }
+
+function allMovies() {
+    fetch('http://localhost:3000/films')
+    .then(response => response.json())
+    .then(movies => {
+        const filmList = document.getElementById('films');
+        filmList.innerHTML = '';
+
+        movies.forEach(movie => {
+            const movieItem = document.createElement('li');
+            movieItem.classList.add('film', 'item');
+            movieItem.innerText = movie.title;
+
+            movieItem.addEventListener('click', () => movieDetailsDisplay(movie));
+
+            filmList.appendChild(movieItem)
+        });
+
+    }
+    )
+    .catch(error => console.error('Error fetching movies', error));
+}
+
+function buyTicket(movie) {
+    const availableTickets = movie.capacity - movie.tickets_sold;
+
+    if (availableTickets > 0) {
+
+        movie.tickets_sold += 1;
+        document.getElementById('ticket-num').innerText = movie.capacity - movie.tickets_sold
+
+        if (movie.capacity = movie.tickets_sold === 0) {
+            const ticketBuyingButton = document.getElementById('buy-ticket');
+            ticketBuyingButton.innerText = 'sold out';
+            ticketBuyingButton.disabled = true
+        }
+
+        fetch(`http://localhost:3000/films/${movie.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ tickets_sold: movie.tickets_sold}),
+
+        })
+        .then(response => response.json())
+        .then(updatedMovie => console.log('Ticket purchase successful:', updatedMovie))
+        .catch(error => console.log('Error updating movie ticket:', error));
+    }
+}
+
+function init() {
+    firstMovie();
+    allMovies();
+}
+
+window.onload = init;
